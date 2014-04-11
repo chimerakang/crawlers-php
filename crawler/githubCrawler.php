27470c0915c69f831fc1f5fb4ca4a35d49c8ceaa
Poyu Chen <poyu677@gmail.com>
2014-04-11 20:51:26 +0800

    finish issue prototype

--- crawler/githubCrawler.php
+++ crawler/githubCrawler.php
@@ -1,8 +1,21 @@
 <?php
     require('issue.php');
-
+/*
     $issueMainPage = file_get_contents('https://github.com/kripken/emscripten/issues'); 
     echo getCloseIssue($issueMainPage) . "\n";
     echo getOpenIssue($issueMainPage) . "\n";
     echo getTotalIssue($issueMainPage) . "\n";
+
+    $authors = array();
+    $issue2265Page = file_get_contents('https://github.com/kripken/emscripten/issues/2265');
+    echo getCommentCountInSingleIssuePage($issue2265Page) . "\n";
+    echo getAuthorCountInSingleIssuePage($issue2265Page, $authors) . "\n";
+
+    $issue2274Page = file_get_contents('https://github.com/kripken/emscripten/issues/2274');
+    echo getCommentCountInSingleIssuePage($issue2274Page) . "\n";
+    echo getAuthorCountInSingleIssuePage($issue2274Page, $authors) . "\n";
+
+    echo sizeof($authors) . "\n";
+*/
+    traverseIssues('https://github.com/kripken/emscripten/issues', 5);
 ?>
--- crawler/issue.php
+++ crawler/issue.php
@@ -18,10 +18,45 @@
     function intStrToInt($str){
         $value = 0;
         for($i = 0; $i < strlen($str); ++$i) {
-            if($str[$i] >= '0' && $str[$i] <= '9') {
+            if($str[$i] >= '0' AND $str[$i] <= '9') {
                 $value = $value*10 + ($str[$i] - '0');
             }
         }
         return $value;
     }
+
+    function traverseIssues($baseUrl, $issueCount){
+        $totalComments = 0;
+        $authors = array();
+        for($i = 1; $i < $issueCount; ++$i) {
+            $html = file_get_contents($baseUrl . "/" . $i);
+            $totalComments += getCommentCountInSingleIssuePage($html);
+            getAuthorCountInSingleIssuePage($html, $authors);
+        }
+        echo $totalComments . "\n";
+        echo sizeof($authors) . "\n";
+    }
+
+    function getCommentCountInSingleIssuePage($html){
+        return preg_match_all('/class="timeline-comment-header-text"/', $html);
+    }
+
+    function getAuthorCountInSingleIssuePage($html, &$authorArr = null){
+        if($authorArr == null)
+            $authorArr = array();
+        $localAuthorArr = array();
+
+        $htmlArr = explode("\n", $html);
+        $i = 0;
+        foreach($htmlArr as $line) {
+            if($line === '        <div class="timeline-comment-header-text">'){
+                $author = trim(strip_tags($htmlArr[$i+2] . $htmlArr[$i+3]));
+                $authorArr[$author] = 0;
+                $localAuthorArr[$author] = 0;
+            }
+            ++$i;
+        }
+        return sizeof($localAuthorArr);
+    }
+
 ?>
